4d087cb26b71b4712d000ca9b676d80380adc689
mservillon <mackservillon@gmail.com>
2023-11-09 19:54:32 -0600

    Adding the create note function

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import { v4 as uuid } from 'uuid'
 import { listNotes } from './graphql/queries'
 import { createNote as CreateNote } from './graphql/mutations'
 
+const CLIENT_ID = uuid()
+
 const initialState = {
   notes: [],
   loading: true,
@@ -19,6 +21,12 @@ function reducer(state, action) {
   switch(action.type) {
     case 'SET_NOTES':
       return { ...state, notes: action.notes, loading: false }
+    case 'ADD_NOTE':
+      return { ...state, notes: [action.note, ...state.notes]}
+    case 'RESET_FORM':
+      return { ...state, form: initialState.form }
+    case 'SET_INPUT':
+      return { ...state, form: { ...state.form, [action.name]: action.value } }
     case 'ERROR':
       return { ...state, loading: false, error: true }
     default:
@@ -40,6 +48,32 @@ const App = () => {
       console.log('error: ', err)
       dispatch({ type: 'ERROR' })
     }
+  }
+
+  const createNote = async() => {
+    const { form } = state // destructuring - form element out of state 
+
+    if (!form.name || !form.description) {
+       return alert('please enter a name and description')
+    }
+
+    const note = { ...form, clientId: CLIENT_ID, completed: false, id: uuid() }
+    dispatch({ type: 'ADD_NOTE', note })
+    dispatch({ type: 'RESET_FORM' })
+
+    try {
+      await API.graphql({
+        query: CreateNote,
+        variables: { input: note }
+      })
+      console.log('successfully created note!')
+    } catch (err) {
+      console.error("error: ", err)
+    }
+  }
+
+  const onChange = (e) => {
+    dispatch({ type: 'SET_INPUT', name: e.target.name, value: e.target.value })
   }
 
   useEffect(() => {
@@ -66,10 +100,28 @@ const App = () => {
 
   return (
     <div style={styles.container}>
+      <Input
+        onChange={onChange}
+        value={state.form.name}
+        placeholder="Note Name"
+        name='name'
+        style={styles.input}
+      />
+      <Input
+        onChange={onChange}
+        value={state.form.description}
+        placeholder="Note description"
+        name='description'
+        style={styles.input}
+      />
+      <Button
+        onClick={createNote}
+        type="primary"
+      >Create Note</Button>
     <List
-      loading={state.loading}
-      dataSource={state.notes}
-      renderItem={renderItem}
+        loading={state.loading}
+        dataSource={state.notes}
+        renderItem={renderItem}
     />
   </div>
   );
